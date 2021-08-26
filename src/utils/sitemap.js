61272443892e716dd50db84671081fcbde4648cb
wfercanas <fernando.canas@outlook.com>
2021-08-26 16:32:27 -0500

    add/ sitemap

---
+++ src/utils/sitemap.js
@@ -0,0 +1,40 @@
+const sitemap = {
+  'Quienes somos': [
+    'Historia',
+    'Identidad',
+    'Evangelización marista',
+    'Proyecto educativo institucional',
+    'Sistema institucional de evaluación',
+    'Proyecto ecológico',
+    'Manual de convivencia',
+    'Protocolos',
+    'Tratamiento de datos',
+  ],
+  Admisiones: ['Pasos del proceso', 'Políticas', 'Información complementaria'],
+  Noticias: ['Boletines', 'Desde el cuarto piso'],
+  Estudiantes: [
+    'Campus virtual',
+    'Calendario académico',
+    'Recursos académicos',
+    'Biblioteca',
+  ],
+  Padres: [
+    'Circulares',
+    'Contacto docente',
+    'Consejo de padres',
+    'Rutas',
+    'Útiles escolares',
+  ],
+  Asofamilia: [],
+  Contacto: ['PQRSF', 'Rutas de atención y denuncia'],
+  Portales: [
+    'Correo marista',
+    'Encuestas maristas',
+    'Indicadores maristas',
+    'Pagos',
+    'Portal docente',
+    'SGC',
+  ],
+};
+
+export { sitemap };
