466944bd514edfa4318a5c3302e18b6b1867899a
wfercanas <fernando.canas@outlook.com>
2021-08-26 10:52:47 -0500

    add/ wrap stories with global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import React from 'react';
+
+import { GlobalStyles } from '../src/styles/GlobalStyles.js';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
