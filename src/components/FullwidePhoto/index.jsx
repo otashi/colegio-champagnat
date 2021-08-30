9fa6374250308d82f7af42a15e29c1291116e6d1
wfercanas <fernando.canas@outlook.com>
2021-08-30 18:48:59 -0500

    add/ children if its a news card with title

--- src/components/FullwidePhoto/index.jsx
+++ src/components/FullwidePhoto/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { PhotoFilter } from '../PhotoFilter';
 import { StyledFullwidePhotoContainer, StyledFullwidePhoto } from './style';
 
-const FullwidePhoto = ({ path }) => {
+const FullwidePhoto = ({ path, children }) => {
   return (
     <StyledFullwidePhotoContainer>
       <PhotoFilter />
       <StyledFullwidePhoto src={path} />
+      {children}
     </StyledFullwidePhotoContainer>
   );
 };
