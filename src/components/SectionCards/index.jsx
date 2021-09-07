f9cfc9a15db8d9941b60e92e9dc3db0d0a7dbc52
wfercanas <fernando.canas@outlook.com>
2021-09-07 16:22:19 -0500

    add/ component for showing multiple cards

---
+++ src/components/SectionCards/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledSectionCards } from './style';
+
+const SectionCards = ({ children }) => {
+  return <StyledSectionCards>{children}</StyledSectionCards>;
+};
+
+export { SectionCards };
---
+++ src/components/SectionCards/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledSectionCards = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+export { StyledSectionCards };
