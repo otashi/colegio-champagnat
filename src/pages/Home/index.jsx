6f039ebae0267bab085ca863aa0a87a39ea4155b
wfercanas <fernando.canas@outlook.com>
2021-09-07 12:40:22 -0500

    add/ homepage layout

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FullwideNews } from '../../components/FullwideNews';
+import { SectionTitle } from '../../components/SectionTitle';
+import { Carousel } from '../../components/Carousel';
+import { PartnerCard } from '../../components/PartnerCard';
+import { Footer } from '../../components/Footer';
+
+import {
+  StyledLastNewsSection,
+  StyledPartnerSection,
+  StyledPartnerSectionTitle,
+  StyledPartnersContainer,
+} from './style';
+
+const Home = () => {
+  return (
+    <>
+      <FullwideNews />
+      <StyledLastNewsSection>
+        <SectionTitle title='Últimas noticias' />
+        <Carousel />
+      </StyledLastNewsSection>
+      <StyledPartnerSection>
+        <StyledPartnerSectionTitle>Alianzas</StyledPartnerSectionTitle>
+        <StyledPartnersContainer>
+          <PartnerCard />
+          <PartnerCard />
+          <PartnerCard />
+          <PartnerCard />
+          <PartnerCard />
+          <PartnerCard />
+        </StyledPartnersContainer>
+      </StyledPartnerSection>
+      <StyledPartnerSection>
+        <StyledPartnerSectionTitle>Sponsors</StyledPartnerSectionTitle>
+        <StyledPartnersContainer>
+          <PartnerCard />
+          <PartnerCard />
+          <PartnerCard />
+        </StyledPartnersContainer>
+      </StyledPartnerSection>
+      <StyledPartnerSection>
+        <StyledPartnerSectionTitle>Redes</StyledPartnerSectionTitle>
+        <StyledPartnersContainer>
+          <PartnerCard />
+        </StyledPartnersContainer>
+      </StyledPartnerSection>
+      <Footer />
+    </>
+  );
+};
+
+export { Home };
