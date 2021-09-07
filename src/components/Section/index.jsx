34b142d3dd88a4b4e022754c8e21f7200182deb7
wfercanas <fernando.canas@outlook.com>
2021-09-07 13:02:44 -0500

    add/ component for main page section

---
+++ src/components/Section/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledSection } from './style';
+
+const Section = ({ children }) => {
+  return <StyledSection>{children}</StyledSection>;
+};
+
+export { Section };
---
+++ src/components/Section/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+  background-color: var(--n10);
+  padding: 1.6rem 1.6rem 3.2rem;
+`;
+
+export { StyledSection };
