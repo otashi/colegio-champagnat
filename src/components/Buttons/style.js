591ab49b3776d6ce3501a64be66b0563f42258d5
wfercanas <fernando.canas@outlook.com>
2021-09-07 16:39:37 -0500

    add/ centering button and vertical margin

--- src/components/Buttons/style.js
+++ src/components/Buttons/style.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
+  display: block;
+  margin: 1.6rem auto;
+  padding: 0.8rem 3.2rem;
   background-color: var(--b400);
   color: var(--n0);
-  padding: 0.8rem 3.2rem;
   border: none;
   border-radius: 0.4rem;
   cursor: pointer;
 `;
 
 export { StyledButton };
- 
