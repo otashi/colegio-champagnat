132e4a24a04f79d3a206c6336720b90835c61181
wfercanas <fernando.canas@outlook.com>
2021-09-07 12:41:04 -0500

    add/ react-router and path to homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Nav } from './components/Nav';
+
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className='App'>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
