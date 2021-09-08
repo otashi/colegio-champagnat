144dc10bd9c3a32028e91f95837a414729259d36
wfercanas <fernando.canas@outlook.com>
2021-09-08 17:50:05 -0500

    add/ component should have the blanket as bground

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -7,25 +7,28 @@ import {
   StyledSubsectionList,
   StyledSubsectionItem,
 } from './style';
+import { Blanket } from '../Blanket';
 
 const Menu = () => {
   const sections = Object.keys(sitemap);
 
   return (
-    <StyledMenu>
-      {sections.map((section) => {
-        return (
-          <StyledSection>
-            <StyledSectionItem>{section}</StyledSectionItem>
-            <StyledSubsectionList>
-              {sitemap[section].map((subsection) => (
-                <StyledSubsectionItem>{subsection}</StyledSubsectionItem>
-              ))}
-            </StyledSubsectionList>
-          </StyledSection>
-        );
-      })}
-    </StyledMenu>
+    <Blanket>
+      <StyledMenu>
+        {sections.map((section) => {
+          return (
+            <StyledSection>
+              <StyledSectionItem>{section}</StyledSectionItem>
+              <StyledSubsectionList>
+                {sitemap[section].map((subsection) => (
+                  <StyledSubsectionItem>{subsection}</StyledSubsectionItem>
+                ))}
+              </StyledSubsectionList>
+            </StyledSection>
+          );
+        })}
+      </StyledMenu>
+    </Blanket>
   );
 };
 
