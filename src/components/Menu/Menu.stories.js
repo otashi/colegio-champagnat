b22428f37270a2d0005d1273a717ebcac5e39cd1
wfercanas <fernando.canas@outlook.com>
2021-09-08 17:49:23 -0500

    Fix/ component should have the blanket, not decor

--- src/components/Menu/Menu.stories.js
+++ src/components/Menu/Menu.stories.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Blanket } from '../Blanket';
 import { Menu } from '.';
 
 export default {
   title: 'Components/Menu',
   component: Menu,
-  decorators: [
-    (Story) => (
-      <Blanket>
-        <Story />
-      </Blanket>
-    ),
-  ],
 };
 
 export const Primary = () => <Menu />;
