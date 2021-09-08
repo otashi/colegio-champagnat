0426ed623d8d3e2cc8b9c75f510fe7a673d69c87
wfercanas <fernando.canas@outlook.com>
2021-09-08 17:53:25 -0500

    add/ nav creates portal and toggles menu display

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import {
   StyledIcon,
   StyledNavLinks,
@@ -10,31 +11,38 @@ import {
 import logo from '../../assets/champagnat.png';
 import '../../styles/icons.css';
 import { sitemap } from '../../utils/sitemap';
+import { Menu } from '../Menu';
 
 const Nav = () => {
   const [menu, setMenu] = useState(false);
+  const [modal, setModal] = useState(false);
   const toggleMenu = () => {
     setMenu(!menu);
+    setModal(!modal);
   };
 
   const sections = Object.keys(sitemap);
 
   return (
-    <StyledNav>
-      <StyledLogo src={logo} alt='Champagnat' />
-      <StyledNavLinks>
-        <StyledNavSectionsList>
-          {sections.map((section) => (
-            <StyledNavSectionItem>{section}</StyledNavSectionItem>
-          ))}
-        </StyledNavSectionsList>
-        {!menu ? (
-          <StyledIcon className='ci-hamburger' onClick={toggleMenu} />
-        ) : (
-          <StyledIcon className='ci-close_big' onClick={toggleMenu} />
-        )}
-      </StyledNavLinks>
-    </StyledNav>
+    <>
+      <StyledNav>
+        <StyledLogo src={logo} alt='Champagnat' />
+        <StyledNavLinks>
+          <StyledNavSectionsList>
+            {sections.map((section) => (
+              <StyledNavSectionItem>{section}</StyledNavSectionItem>
+            ))}
+          </StyledNavSectionsList>
+          {!menu ? (
+            <StyledIcon className='ci-hamburger' onClick={toggleMenu} />
+          ) : (
+            <StyledIcon className='ci-close_big' onClick={toggleMenu} />
+          )}
+        </StyledNavLinks>
+      </StyledNav>
+      {modal &&
+        ReactDOM.createPortal(<Menu />, document.getElementById('modal'))}
+    </>
   );
 };
 
