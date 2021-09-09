c1b95723fb3544e214df40a062bd2d5b6438a736
wfercanas <fernando.canas@outlook.com>
2021-09-08 19:34:36 -0500

    add/ dummy news for confirming proper render

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FullwideNews } from '../../components/FullwideNews';
 import { SectionTitle } from '../../components/SectionTitle';
 import { Carousel } from '../../components/Carousel';
@@ -12,12 +12,33 @@ import {
 } from './style';
 
 const Home = () => {
+  const [news] = useState([
+    {
+      title: 'Title of the article',
+      category: 'Category',
+      text:
+        'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia....',
+    },
+    {
+      title: 'Title of the article',
+      category: 'Category',
+      text:
+        'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia....',
+    },
+    {
+      title: 'Title of the article',
+      category: 'Category',
+      text:
+        'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia....',
+    },
+  ]);
+
   return (
     <>
       <FullwideNews />
       <StyledLastNewsSection>
         <SectionTitle title='Últimas noticias' />
-        <Carousel />
+        <Carousel news={news} />
       </StyledLastNewsSection>
       <StyledPartnerSection>
         <StyledPartnerSectionTitle>Alianzas</StyledPartnerSectionTitle>
