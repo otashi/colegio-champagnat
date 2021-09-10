1a21499e88a702e438bc6d6ea56d37128c293602
wfercanas <fernando.canas@outlook.com>
2021-09-10 10:26:10 -0500

    add/ get news data through props

--- src/components/FullwideNews/index.jsx
+++ src/components/FullwideNews/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { FullwidePhoto } from '../FullwidePhoto';
+import { Link } from '../Link';
 import {
   StyledFullwideNewsData,
   StyledFullwideNewsTitle,
   StyledFullwideNewsCategory,
 } from './style';
 
-const FullwideNews = ({ title, category, photo }) => {
-  return (
-    <FullwidePhoto coverURL={photo}>
-      <StyledFullwideNewsData>
-        <StyledFullwideNewsTitle>{title}</StyledFullwideNewsTitle>
-        <StyledFullwideNewsCategory>{category}</StyledFullwideNewsCategory>
-      </StyledFullwideNewsData>
-    </FullwidePhoto>
-  );
+const FullwideNews = ({ news }) => {
+  if (news.length >= 1) {
+    return (
+      <Link to='/'>
+        <FullwidePhoto coverURL={news[0].coverURL}>
+          <StyledFullwideNewsData>
+            <StyledFullwideNewsTitle>{news[0].title}</StyledFullwideNewsTitle>
+            <StyledFullwideNewsCategory>
+              {news[0].category.category}
+            </StyledFullwideNewsCategory>
+          </StyledFullwideNewsData>
+        </FullwidePhoto>
+      </Link>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export { FullwideNews };
