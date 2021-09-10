9d4674d2cea650917d0eea6a487a0ad6468c51a4
wfercanas <fernando.canas@outlook.com>
2021-09-10 12:10:52 -0500

    add/ render page content received through props

--- src/pages/About/QuienesSomos/index.jsx
+++ src/pages/About/QuienesSomos/index.jsx
@@ -4,25 +4,29 @@ import { QuickNav } from '../../../components/QuickNav';
 import { Section } from '../../../components/Section';
 import { SectionTitle } from '../../../components/SectionTitle';
 
-const QuienesSomos = () => {
-  return (
-    <>
-      <FullwidePhoto />
-      <QuickNav section='Quienes somos' />
-      <Section>
-        <SectionTitle title='Quiénes somos' />
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </p>
-      </Section>
-    </>
-  );
+const QuienesSomos = ({ content = [] }) => {
+  if (content.length >= 1) {
+    return (
+      <>
+        <FullwidePhoto coverURL={content[0].coverURL} />
+        <QuickNav section='Quienes somos' />
+        <Section>
+          <SectionTitle title='Quiénes somos' />
+          <p>{content[0].content}</p>
+        </Section>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <FullwidePhoto />
+        <QuickNav section='Quienes somos' />
+        <Section>
+          <SectionTitle title='Quiénes somos' />
+        </Section>
+      </>
+    );
+  }
 };
 
 export { QuienesSomos };
