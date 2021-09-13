7f4b6c2d035f60f3c71b19eebd3c2526e128bce3
wfercanas <fernando.canas@outlook.com>
2021-09-13 17:05:03 -0500

    add/ mqueries for max-width and centered form

--- src/pages/Contact/PQRSF/style.js
+++ src/pages/Contact/PQRSF/style.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
 
 const StyledForm = styled.form`
-  margin: 3.2rem 0;
   display: grid;
+  justify-content: center;
+  margin: 3.2rem 0;
   row-gap: 0.8rem;
+
+  @media screen and (min-width: 340px) {
+    grid-template-columns: 34rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: 46rem;
+  }
 `;
 
 export { StyledForm };
