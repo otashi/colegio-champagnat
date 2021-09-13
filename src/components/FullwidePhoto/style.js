eb62789fc4853df4f305b421e879a8df6e479188
wfercanas <fernando.canas@outlook.com>
2021-09-13 16:39:33 -0500

    add/ titular news mquery for desktop centered

--- src/components/FullwidePhoto/style.js
+++ src/components/FullwidePhoto/style.js
@@ -2,10 +2,16 @@ import styled from 'styled-components';
 
 const StyledFullwidePhotoContainer = styled.div`
   position: relative;
+  display: block;
   width: 100vw;
   height: 62vw;
   max-width: 1026px;
   max-height: 634px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1026px) {
+    margin: 3.2rem auto 0;
+  }
 `;
 
 const StyledFullwidePhoto = styled.div`
