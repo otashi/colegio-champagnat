717bf124c6aed537ff4b5977798dba71024e4230
wfercanas <fernando.canas@outlook.com>
2021-09-13 11:18:37 -0500

    add/ button as an anchor element

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Anchor } from '../Anchor';
 import { StyledButton } from './style';
 
-const Button = ({ label, handleClick }) => {
-  return <StyledButton onClick={handleClick}>{label}</StyledButton>;
+const Button = ({ label, handleClick, type, href }) => {
+  if (type === 'Anchor') {
+    return (
+      <StyledButton>
+        <Anchor href={href}>{label}</Anchor>
+      </StyledButton>
+    );
+  } else {
+    return <StyledButton onClick={handleClick}>{label}</StyledButton>;
+  }
 };
 
 export { Button };
