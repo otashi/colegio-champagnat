8ef75cb3babf6def87ec820a2c092c36133519fd
wfercanas <fernando.canas@outlook.com>
2021-09-13 18:03:21 -0500

    add/ max-width for sections in desktop

--- src/components/Section/style.js
+++ src/components/Section/style.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 const StyledSection = styled.section`
   background-color: var(--n10);
   padding: 1.6rem 1.6rem 3.2rem;
+  max-width: 1024px;
+  margin: 0 auto;
 `;
 
 export { StyledSection };
